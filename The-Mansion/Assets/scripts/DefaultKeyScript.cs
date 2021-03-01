20c30a1687efc790fd10ee329eb596bbb1d69f57
Leon Boussen <leon.boussen@hotmail.com>
2021-03-01 17:10:59 +0100

    keymodel

---
+++ The-Mansion/Assets/scripts/DefaultKeyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultKeyScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("This a Hit");
+
+        public class PlayerInv : MonoBehaviour
+    {
+        public MainDoorKey
+
+        MainDoorKey = true;
+    }
+    }
+}
+
