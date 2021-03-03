6714582e11ef3973db1eaaf6a0eab8a6ee91d533
Timo Zandbergen <zandbergentimo@gmail.com>
2021-03-03 13:54:30 +0100

    flaslight

---
+++ The-Mansion/Assets/Flashlight/Flaslight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flaslight : MonoBehaviour
+{
+    public AudioClip soundOn;
+    public AudioClip soundOff;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (light.enabled == false)
+
+        {
+                 light.enabled == true;
+                 audio.clip = soundOn;
+                 audio.Play
+        }
+            {
+            else
+            }
+
+            {
+                light.enabled == false;
+                audio.clip = soundOff;
+                audio.Play
+            }
+
+    }
+}
+
+}
